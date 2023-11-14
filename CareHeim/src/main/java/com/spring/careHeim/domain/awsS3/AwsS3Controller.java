fdb3654ca2bc25a0adb98c0bf091feb57dbb109d
zzo3ozz <hhzzo@DESKTOP-VCOTNUF>
2023-11-14 18:51:30 +0900

    refactor: annotation 추가에 따른 initializer 삭제

--- CareHeim/src/main/java/com/spring/careHeim/domain/awsS3/AwsS3Controller.java
+++ CareHeim/src/main/java/com/spring/careHeim/domain/awsS3/AwsS3Controller.java
@@ -4,17 +4,19 @@ import com.spring.careHeim.config.BaseException;
 import com.spring.careHeim.config.BaseResponse;
 import com.spring.careHeim.config.BaseResponseStatus;
 import com.spring.careHeim.domain.awsS3.model.S3Object;
+import com.spring.careHeim.domain.clothes.ClotheService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/s3")
 public class AwsS3Controller {
     private final AwsS3Service awsS3Service;
-
-    public AwsS3Controller(AwsS3Service awsS3Service) {
-        this.awsS3Service = awsS3Service;
-    }
 
     @ResponseBody
     @PostMapping("/image")
